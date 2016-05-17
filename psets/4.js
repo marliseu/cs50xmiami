f905c41cf5be0dbabe3fbdb1bc8f0f3021503b7a
marliseu <marlise.au@gmail.com>
2016-05-17 19:28:29 -0400

    Updated prompt for node

--- psets/4.js
+++ psets/4.js
@@ -11,12 +11,23 @@ http://www.mathsisfun.com/temperature-conversion.html
 */
 
 // write your solution here...
-var celsius = prompt("What is the temperature in Celsius?");
-//Multiply by 9, then divide by 5, then add 32
-var farenheit = ((celsius * 9) / 5) + 32;
-console.log(celsius + " ºC is " + farenheit + "ºF");
+var prompt = require('prompt');
 
-farenheit = prompt("What is the temperature in Farenheit?");
-//Deduct 32, then multiply by 5, then divide by 9
-celsius = ((farenheit - 32) * 5) / 9;
-console.log(farenheit + " ºF is " + celsius + "ºC");
+  prompt.start();
+
+  prompt.get(['celsius', 'farenheit'], function (err, result) {
+    var celsius = result.celsius;
+    //Multiply by 9, then divide by 5, then add 32
+    var farenheit = ((celsius * 9) / 5) + 32;
+    console.log(celsius + " ºC is " + farenheit + "ºF");
+
+    farenheit = result.farenheit;
+    //Deduct 32, then multiply by 5, then divide by 9
+    celsius = ((farenheit - 32) * 5) / 9;
+    console.log(farenheit + " ºF is " + celsius + "ºC");
+  });
+
+  function onErr(err) {
+    console.log(err);
+    return 1;
+  }

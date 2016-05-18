b82e4f8475ea3a2ad183946d828e06d08ef4fdf7
marliseu <marlise.au@gmail.com>
2016-05-17 20:47:12 -0400

    Solved

--- psets/8.js
+++ psets/8.js
@@ -18,4 +18,32 @@ For example:
 
 function verbing(word) {
   // write your solution here...
+  var len = word.length;
+  if (len < 3) {
+    return word;
+  }
+  else {
+    var index = word.search("ing");
+    if (index > 0) {
+      return word + "ly";
+    }
+    else {
+      return word + word[len-1] + "ing";
+    }
+  }
 }
+
+var prompt = require('prompt');
+
+  prompt.start();
+
+  prompt.get(['word'], function (err, result) {
+    // Code here
+    var word = result.word;
+    console.log(verbing(word));
+  });
+
+  function onErr(err) {
+    console.log(err);
+    return 1;
+  }

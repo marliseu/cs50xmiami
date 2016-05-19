e195071e4afe31635dcb0be8df42c59f1e7d187d
marliseu <marlise.au@gmail.com>
2016-05-19 19:06:26 -0400

    Solved

--- psets/10.js
+++ psets/10.js
@@ -10,3 +10,24 @@ Write a function named tellFortune that:
 */
 
 // write your solution here...
+function tellFortune(children, partner, geo, job) {
+  console.log("You will be a " + job + " in " + geo + ", and married to " + partner + " with " + children + " kids.")
+}
+
+// console.log(tellFortune(0, 'no one', 'the world', 'person'));
+// console.log(tellFortune(2, 'a hipster', 'Oregon', 'barista'));
+// console.log(tellFortune(4, 'a rich dude', 'New York', 'trophy wife'));
+
+var prompt = require('prompt');
+
+  prompt.start();
+
+  prompt.get(['number', 'person', 'place', 'job'], function (err, result) {
+    // Code here
+    console.log(tellFortune(result.number, result.person, result.place, result.job));
+  });
+
+  function onErr(err) {
+    console.log(err);
+    return 1;
+  }
